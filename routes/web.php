545381d8fccb47b4c2cd3893d96096d21eea44f2
OliveiraAnna99 <anna.oliveira.700@ufrn.edu.br>
2023-05-06 10:18:20 -0300

    sigres: adicionando template (#38)

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
