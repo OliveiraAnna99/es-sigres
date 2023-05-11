84f47dec038f08f35af93b2c829ec444c1ab0a86
israelsilva282 <israelcostas@hotmail.com>
2023-05-10 21:54:08 -0300

    feat: adicionado html do index funcionarios (#47)

--- resources/views/funcionarios/index.blade.php
+++ resources/views/funcionarios/index.blade.php
@@ -1,3 +1,29 @@
 @extends('home')
+
+@section('title', __('funcionario.list'))
+
 @section('content')
+
+<div class="funcionarioSection">
+    <div class="topSection">
+        <a href="{{ route('funcionarios.create') }}" class="btnAddFunc">
+            <x-bi-plus-circle class="icon" />
+            <p>Adicionar funcionário</p>
+        </a>
+
+
+    </div>
+    <div class="card">
+        @foreach($funcionarios as $key => $funcionario)
+        <div class="funcSection">
+            <p class="funcName">{{$funcionario->nome}}</´>
+            <p>{{$funcionario->funcao}}</p>
+            <a href="{{ route('funcionarios.show', $funcionario) }}" class="btn btnShow">
+                <p>ver</p>
+            </a>
+        </div>
+        @endforeach
+    </div>
+</div>
+
 @endsection
