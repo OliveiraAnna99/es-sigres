f7081ddd69561d21989d3a955547e6ccc66e00ba
israelsilva282 <israelcostas@hotmail.com>
2023-05-11 20:56:00 -0300

    fix: consertado erro ao clicar no botao de editar funcionario (#53)

--- resources/views/funcionarios/edit.blade.php
+++ resources/views/funcionarios/edit.blade.php
@@ -65,7 +65,6 @@
     </div>
 </div>
 </div>
-@endif
 @endsection
 <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js" integrity="sha256-u7MY6EG5ass8JhTuxBek18r5YG6pllB9zLqE4vZyTn4=" crossorigin="anonymous"></script>
