cb4ba2658f13a9e1d309e955e61bb473134aa9ef
israelsilva282 <israelcostas@hotmail.com>
2023-05-10 21:50:35 -0300

    feat: adicionado html do sidebar (#47)

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,3 +1,17 @@
-<div>
-    <!-- Do what you can, with what you have, where you are. - Theodore Roosevelt -->
+<div class="sidebarContainer">
+    <div class="profileSection">
+        <p>Israel</p>
+    </div>
+
+    <nav class="selectContainer">
+        <a class="navContainer">
+            <x-bi-cart-fill class="icon" />
+            <p>Pedidos</p>
+        </a>
+        <a class="navContainer" href="{{ route('funcionarios.index') }}">
+            <x-bi-person-fill class="icon" />
+            <p>Funcionários</p>
+        </a>
+    </nav>
+
 </div>
