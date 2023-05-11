b17b86b5e6231e225aa263bcfd2584483b41e95f
israelsilva282 <israelcostas@hotmail.com>
2023-05-10 21:46:43 -0300

    feat: adicionado componente sidebar (#47)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,15 +6,18 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
 
-    <title>Sigres</title>
+    <title>SigRestaurant</title>
+
+    <link rel="stylesheet" href="/css/styles.css">
 
 </head>
 
-<body id="page-top">
-
+<body>
+    <div class="container">
+        <x-sidebar></x-sidebar>
+        @yield('content')
+    </div>
 </body>
 
 </html>
