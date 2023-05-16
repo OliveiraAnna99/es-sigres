df1fa95c964b07b3cb35ec1ec3cf13f2d77b38eb
isazvdd <isadora.azevedo.700@ufrn.edu.br>
2023-05-16 16:03:03 -0300

    feat: adicionando css na página de adicionar novos produtos ao estoque (#57)

--- resources/views/estoques/create.blade.php
+++ resources/views/estoques/create.blade.php
@@ -1,3 +1,9 @@
+@extends('home')
+
+@section('title', __('estoque.detail'))
+
+@section('content')
+
 <div class="cardContainer">
     <div class="cardContainerContent cardCreate">
         <form method="POST" action="{{ route('estoques.store') }}">
@@ -5,31 +11,31 @@
 
             <div class="cardBody">
                 <div class="formGroup">
-                    <label for="nome">{{ __('estoque.name') }} <span>*</span></label>
-                    <input id="nome" type="text" class="form-control{{ $errors->has('nome') ? ' is-invalid' : '' }}" name="nome" value="{{ old('nome') }}" required>
-                    {!! $errors->first('nome', '<span class="invalid-feedback" role="alert">:message</span>') !!}
+                    <label for="item">{{ __('estoque.item') }} <span>*</span></label>
+                    <input id="item" type="text" class="form-control{{ $errors->has('nome') ? ' is-invalid' : '' }}" name="item" value="{{ old('item') }}" required>
+                    {!! $errors->first('item', '<span class="invalid-feedback" role="alert">:message</span>') !!}
+                </div>
+
+                <div class="formGroup ">
+                    <div>
+                        <label for="quant">{{ __('estoque.quant') }} <span>*</span></label>
+                        <input id="quant" type="number" min="1" class="form-control{{ $errors->has('nome') ? ' is-invalid' : '' }}" name="quant" value="{{ old('quant') }}" required>
+                        {!! $errors->first('quant', '<span class="invalid-feedback" role="alert">:message</span>') !!}
+                    </div>
+                </div>
+
+                <div class="formGroup">
+                    <label for="date">{{ __('estoque.date') }} <span>*</span></label>
+                    <input id="date" type="date" min="1" class="form-control{{ $errors->has('nome') ? ' is-invalid' : '' }}" name="date" value="{{ old('date') }}" required>
+                    {!! $errors->first('date', '<span class="invalid-feedback" role="alert">:message</span>') !!}
                 </div>
 
                 <div class="cardFooter">
                     <input type="submit" value="{{ __('estoque.create') }}" class="btn btnSecondary">
                     <a href="{{ route('estoques.index') }}" class="btn btnPrimary">{{ __('estoque.cancel') }}</a>
                 </div>
-
         </form>
+
     </div>
 </div>
 @endsection
-<!-- <script src="https://code.jquery.com/jquery-3.3.1.min.js"
-  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js" integri
-  ty="sha256-u7MY6EG5ass8JhTuxBek18r5YG6pllB9zLqE4vZyTn4=" crossorigin="anonymous"></script> -->
-
-<!-- <script type="text/javascript">
-    $(document).ready(function($) {
-
-        $('#cpf').mask('000.000.000-00');
-        $('#contato').mask('(00) 00000-0000');
-        $('#rg').mask('000.000.000');
-
-    });
-</script> -->
