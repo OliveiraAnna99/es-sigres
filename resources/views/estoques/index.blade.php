582f1df88b0116fba1cd9c7ea1242f70e456e0dc
isazvdd <isadora.azevedo.700@ufrn.edu.br>
2023-05-17 15:11:51 -0300

    feat: adicionando dados da tabela no index (#57)

--- resources/views/estoques/index.blade.php
+++ resources/views/estoques/index.blade.php
@@ -6,25 +6,25 @@
 
 <div class="section">
 
-    <a href="{{ route('estoques.create') }}" class="btn btnPrimary btnAdd">
-        <x-bi-plus-circle class="icon" />
-        <p>Adicionar produto</p>
-    </a>
+  <a href="{{ route('estoques.create') }}" class="btn btnPrimary btnAdd">
+    <x-bi-plus-circle class="icon" />
+    <p>Adicionar produto</p>
+  </a>
 
 
 </div>
-<!-- <div class="topSection">
-    <div class="card">
-        @foreach($estoques as $key => $estoque)
-        <div class="funcSection">
-            <p class="funcName">{{$estoque->nome}}</´>
-            <p>{{$estoque->funcao}}</p>
-            <a href="{{ route('estoques.show', $estoque) }}" class="btn btnPrimary btnShow">
-                <p>ver</p>
-            </a>
-        </div>
-        @endforeach
-    </div> -->
+<div class="topSection">
+  <div class="card">
+    @foreach($estoques as $key => $estoque)
+    <div class="funcSection">
+      <p class="funcName">{{$estoque->item}}</´>
+      <p>{{$estoque->funcao}}</p>
+      <a href="{{ route('estoques.show', $estoque) }}" class="btn btnPrimary btnShow">
+        <p>ver</p>
+      </a>
+    </div>
+    @endforeach
+  </div>
 </div>
 
 @endsection
--- resources/views/funcionarios/index.blade.php
+++ resources/views/funcionarios/index.blade.php
@@ -1,29 +1,29 @@
-@extends('home')
+x@extends('home')
 
 @section('title', __('funcionario.list'))
 
 @section('content')
 
 <div class="section">
-    <div class="topSection">
-        <a href="{{ route('funcionarios.create') }}" class="btn btnPrimary btnAdd">
-            <x-bi-plus-circle class="icon" />
-            <p>Adicionar funcionário</p>
-        </a>
+  <div class="topSection">
+    <a href="{{ route('funcionarios.create') }}" class="btn btnPrimary btnAdd">
+      <x-bi-plus-circle class="icon" />
+      <p>Adicionar funcionário</p>
+    </a>
 
 
+  </div>
+  <div class="card">
+    @foreach($funcionarios as $key => $funcionario)
+    <div class="funcSection">
+      <p class="funcName">{{$funcionario->nome}}</´>
+      <p>{{$funcionario->funcao}}</p>
+      <a href="{{ route('funcionarios.show', $funcionario) }}" class="btn btnPrimary btnShow">
+        <p>ver</p>
+      </a>
     </div>
-    <div class="card">
-        @foreach($funcionarios as $key => $funcionario)
-        <div class="funcSection">
-            <p class="funcName">{{$funcionario->nome}}</´>
-            <p>{{$funcionario->funcao}}</p>
-            <a href="{{ route('funcionarios.show', $funcionario) }}" class="btn btnPrimary btnShow">
-                <p>ver</p>
-            </a>
-        </div>
-        @endforeach
-    </div>
+    @endforeach
+  </div>
 </div>
 
 @endsection
