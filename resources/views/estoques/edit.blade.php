8c371ffaf8ab463b519d0aea483222f9d1103b16
isazvdd <isadora.azevedo.700@ufrn.edu.br>
2023-05-18 08:58:51 -0300

    feat: alterando página de edit (#57)

--- resources/views/estoques/edit.blade.php
+++ resources/views/estoques/edit.blade.php
@@ -3,63 +3,47 @@
 @section('title', __('estoque.edit'))
 
 @section('content')
-<div class="row justify-content-center">
-  <div class="col-md-6">
-    @if (request('action') == 'delete' && $estoque)
-    @can('delete', $estoque)
-    <div class="card">
-      <div class="card-header">{{ __('estoque.delete') }}</div>
-      <div class="card-body">
-        <label class="form-label text-primary">{{ __('estoque.name') }}</label>
-        <p>{{ $estoque->name }}</p>
-        <label class="form-label text-primary">{{ __('estoque.description') }}</label>
-        <p>{{ $estoque->description }}</p>
-        {!! $errors->first('estoque_id', '<span class="invalid-feedback" role="alert">:message</span>') !!}
-      </div>
-      <hr style="margin:0">
-      <div class="card-body text-danger">{{ __('estoque.delete_confirm') }}</div>
-      <div class="card-footer">
-        <form method="POST" action="{{ route('estoques.destroy', $estoque) }}" accept-charset="UTF-8"
-          onsubmit="return confirm(&quot;{{ __('app.delete_confirm') }}&quot;)" class="del-form float-right"
-          style="display: inline;">
-          {{ csrf_field() }} {{ method_field('delete') }}
-          <input name="estoque_id" type="hidden" value="{{ $estoque->id }}">
-          <button type="submit" class="btn btn-danger">{{ __('app.delete_confirm_button') }}</button>
-        </form>
-        <a href="{{ route('estoques.edit', $estoque) }}" class="btn btn-link">{{ __('app.cancel') }}</a>
-      </div>
-    </div>
-    @endcan
-    @else
-    <div class="card">
-      <div class="card-header">{{ __('estoque.edit') }}</div>
-      <form method="POST" action="{{ route('estoques.update', $estoque) }}" accept-charset="UTF-8">
-        {{ csrf_field() }} {{ method_field('patch') }}
-        <div class="card-body">
-          <div class="form-group">
-            <label for="name" class="form-label">{{ __('estoque.name') }} <span class="form-required">*</span></label>
-            <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name"
-              value="{{ old('name', $estoque->name) }}" required>
-            {!! $errors->first('name', '<span class="invalid-feedback" role="alert">:message</span>') !!}
-          </div>
-          <div class="form-group">
-            <label for="description" class="form-label">{{ __('estoque.description') }}</label>
-            <textarea id="description" class="form-control{{ $errors->has('description') ? ' is-invalid' : '' }}"
-              name="description" rows="4">{{ old('description', $estoque->description) }}</textarea>
-            {!! $errors->first('description', '<span class="invalid-feedback" role="alert">:message</span>') !!}
-          </div>
+<div class="cardContainer">
+  <div class="cardContainerContent cardCreate">
+    <form method="POST" action="{{ route('estoques.update', $estoque) }}" accept-charset="UTF-8">
+      {{ csrf_field() }} {{ method_field('patch') }}
+      <div class="cardBody">
+        <div class="formGroup">
+          <label for="item" class="form-label">{{ __('estoque.item') }} <span class="form-required">*</span></label>
+          <input id="item" type="text" class="form-control{{ $errors->has('item') ? ' is-invalid' : '' }}" name="item"
+            value="{{ $estoque->item }}" required>
+          {!! $errors->first('item', '<span class="invalid-feedback" role="alert">:message</span>') !!}
         </div>
-        <div class="card-footer">
-          <input type="submit" value="{{ __('estoque.update') }}" class="btn btn-success">
-          <a href="{{ route('estoques.show', $estoque) }}" class="btn btn-link">{{ __('app.cancel') }}</a>
-          @can('delete', $estoque)
-          <a href="{{ route('estoques.edit', [$estoque, 'action' => 'delete']) }}" id="del-estoque-{{ $estoque->id }}"
-            class="btn btn-danger float-right">{{ __('app.delete') }}</a>
-          @endcan
+        <div class="formGroup">
+          <label for="quant" class="form-label">{{ __('estoque.quant') }} <span class="form-required">*</span></label>
+          <input id="quant" type="text" class="form-control{{ $errors->has('quant') ? ' is-invalid' : '' }} "
+            name="quant" value="{{ $estoque->quant }}" required>
+          {!! $errors->first('quant', '<span class="invalid-feedback" role="alert">:message</span>') !!}
         </div>
-      </form>
-    </div>
+        <div class="formGroup">
+          <label for="date" class="form-label">{{ __('estoque.date') }} <span class="form-required">*</span></label>
+          <input id="date" type="text" class="form-control{{ $errors->has('date') ? ' is-invalid' : '' }} " name="date"
+            value="{{ $estoque->date }}" required>
+          {!! $errors->first('date', '<span class="invalid-feedback" role="alert">:message</span>') !!}
+        </div>
+
+
+      </div>
+      <div class="cardFooter">
+        <input type="submit" value="{{ __('estoque.update') }}" class="btn btnSecondary">
+        <a href="{{ route('estoques.show', $estoque) }}" class="btn btnPrimary">{{ __('estoque.cancel') }}</a>
+      </div>
+    </form>
   </div>
+
 </div>
-@endif
+</div>
+
+
+
+
 @endsection
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"
+  integrity="sha256-u7MY6EG5ass8JhTuxBek18r5YG6pllB9zLqE4vZyTn4=" crossorigin="anonymous"></script>
