55a11a4b15647eefe8ad63878f9cc856d9f6541b
isazvdd <isadora.azevedo.700@ufrn.edu.br>
2023-05-28 21:46:47 -0300

    bug: criando função de verificação de CPF (#76)

--- resources/views/funcionarios/create.blade.php
+++ resources/views/funcionarios/create.blade.php
@@ -61,16 +61,66 @@
     </div>
 </div>
 @endsection
+
+@section('scripts')
+
 <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js" integrity="sha256-u7MY6EG5ass8JhTuxBek18r5YG6pllB9zLqE4vZyTn4=" crossorigin="anonymous"></script>
 
-<script type="text/javascript">
-    $(document).ready(function($) {
+<script>
+    function validarCPF(cpf) {
+        cpf = cpf.replace(/[^\d]/g, '');
 
+        if (cpf.length !== 11) {
+            return false;
+        }
+
+        if (/(\d)\1{10}/.test(cpf)) {
+            return false;
+        }
+
+        var soma = 0;
+        for (var i = 0; i < 9; i++) {
+            soma += parseInt(cpf.charAt(i)) * (10 - i);
+        }
+        var dv1 = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+
+        soma = 0;
+        for (var j = 0; j < 10; j++) {
+            soma += parseInt(cpf.charAt(j)) * (11 - j);
+        }
+        var dv2 = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+
+        if (dv1 !== parseInt(cpf.charAt(9)) || dv2 !== parseInt(cpf.charAt(10))) {
+            return false;
+        }
+
+        return true;
+    }
+
+    function validarFormulario() {
+        var cpfInput = document.getElementById('cpf');
+        var cpf = cpfInput.value.replace(/[^\d]/g, ''); // Remover caracteres especiais do CPF
+
+        if (cpf.length !== 11 || !validarCPF(cpf)) {
+            alert('CPF inválido');
+            cpfInput.focus();
+            return false;
+        }
+
+        return true;
+    }
+
+    $(document).ready(function() {
         $('#cpf').mask('000.000.000-00');
         $('#contato').mask('(00) 00000-0000');
         $('#rg').mask('000.000.000');
 
-
+        $('form').on('submit', function(e) {
+            if (!validarFormulario()) {
+                e.preventDefault(); // Impedir o envio do formulário se o CPF for inválido
+            }
+        });
     });
 </script>
+@endsection
