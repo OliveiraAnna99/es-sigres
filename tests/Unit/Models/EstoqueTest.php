c9bcc961d553d1d5dbbdc1f8221b184f18f2b040
Oliveira Anna <anna.oliveira.700@ufrn.edu.br>
2023-06-06 18:59:41 -0300

    feat: ajustes nos testes unitarios (#92)

--- tests/Unit/Models/EstoqueTest.php
+++ tests/Unit/Models/EstoqueTest.php
@@ -1,63 +1,38 @@
 <?php
 
-namespace Tests\Unit;
+namespace Tests\Unit\Models;
 
-use Tests\TestCase;
-use App\Models\Estoque;
 use App\Models\User;
+use App\Models\Estoque;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\BrowserKitTest as TestCase;
 
 class EstoqueTest extends TestCase
 {
     use RefreshDatabase;
 
     /** @test */
-    public function it_can_create_a_funcionario()
-    {
-        
-
-        $data = [
-            'item' => 'Item Teste',
-            'quant' => 10,
-            'dataNascimento' => '1990-01-01'
-        ];
-
-        $estoque = Estoque::create($data);
-
-        $this->assertInstanceOf(Estoque::class, $estoque);
-        $this->assertEquals($data['item'], $estoque->item);
-        $this->assertEquals($data['quant'], $estoque->quant);
-        $this->assertEquals($data['dataNascimento'], $estoque->dataNascimento);
-     
-
-    }
-
-    /** @test */
-    public function it_can_update_a_funcionario()
+    public function a_estoque_has_name_link_attribute()
     {
         $estoque = Estoque::factory()->create();
 
-        $data = [
-            'item' => 'Item Teste',
-            'quant' => 10,
-            'dataNascimento' => '1990-01-01'
-        ];
+        $title = __('app.show_detail_title', [
+            'name' => $estoque->name, 'type' => __('estoque.estoque'),
+        ]);
+        $link = '<a href="'.route('estoques.show', $estoque).'"';
+        $link .= ' title="'.$title.'">';
+        $link .= $estoque->name;
+        $link .= '</a>';
 
-        $estoque->update($data);
-
-        $this->assertEquals($data['item'], $estoque->item);
-        $this->assertEquals($data['quant'], $estoque->quant);
-        $this->assertEquals($data['dataNascimento'], $estoque->dataNascimento);
-     
+        $this->assertEquals($link, $estoque->name_link);
     }
 
     /** @test */
-    public function it_can_delete_a_funcionario()
+    public function a_estoque_has_belongs_to_creator_relation()
     {
-        $estoque = Estoque::factory()->create();
+        $estoque = Estoque::factory()->make();
 
-        $estoque->delete();
-
-        $this->assertDeleted($estoque);
+        $this->assertInstanceOf(User::class, $estoque->creator);
+        $this->assertEquals($estoque->creator_id, $estoque->creator->id);
     }
 }
