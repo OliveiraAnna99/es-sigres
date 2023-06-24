98d9ef46bd969c18c13a86d0c4786f41146688e1
Oliveira Anna <anna.oliveira.700@ufrn.edu.br>
2023-06-24 12:05:41 -0300

    feat: criando arquivo de request de cardapio (#113)

--- app/Http/Controllers/CardapioController.php
+++ app/Http/Controllers/CardapioController.php
@@ -4,14 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\Cardapio;
 use Illuminate\Http\Request;
-
+use App\Http\Requests\CardapioRequest;
 class CardapioController extends Controller
 {
-    /**
-     * Display a listing of the cardapio.
-     *
-     * @return \Illuminate\View\View
-     */
+    
     public function index()
     {
         $cardapioQuery = Cardapio::query();
@@ -21,43 +17,19 @@ class CardapioController extends Controller
         return view('cardapios.index', compact('cardapios'));
     }
 
-    /**
-     * Show the form for creating a new cardapio.
-     *
-     * @return \Illuminate\View\View
-     */
+ 
     public function create()
     {
-        //$this->authorize('create', new Cardapio);
 
         return view('cardapios.create');
     }
 
-    /**
-     * Store a newly created cardapio in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Routing\Redirector
-     */
-    public function store(Request $request)
+   
+    public function store(CardapioRequest $request)
     {
-        //$this->authorize('create', new Cardapio);
-        $newCardapio = $request->validate([
-            'nome'         => 'required|max:60',
-            'valor'        => 'required|regex:/\d{1,6}(\,\d{0,2})/',
-            'ingredientes' => 'required|max:500',
-            'status'       => 'required|in:pendente,pronto',
-            // 'imagem'       => 'required|mimes:jpeg,jpg,png',
-            // Adiciona a validação para imagem (opcional e tamanho máximo de 2MB)
-        ]);
+        $newCardapio = $request->validated();
         
         $newCardapio['valor'] = str_replace(',', '.', $newCardapio['valor']);
-        
-        // if ($request->hasFile('imagem')) {
-        //     $imagemPath = $request->file('imagem')->store('imagens', 'public');
-        //     $newCardapio['imagem'] = $imagemPath;
-        // }
-        
         $cardapio = Cardapio::create($newCardapio);
         
         return redirect()->route('cardapios.show', $cardapio);
@@ -65,50 +37,23 @@ class CardapioController extends Controller
     }
     
 
-    /**
-     * Display the specified cardapio.
-     *
-     * @param  \App\Models\Cardapio  $cardapio
-     * @return \Illuminate\View\View
-     */
     public function show(Cardapio $cardapio)
     {
         return view('cardapios.show', compact('cardapio'));
     }
 
-    /**
-     * Show the form for editing the specified cardapio.
-     *
-     * @param  \App\Models\Cardapio  $cardapio
-     * @return \Illuminate\View\View
-     */
+  
     public function edit(Cardapio $cardapio)
     {
-        //$this->authorize('update', $cardapio);
-
         return view('cardapios.edit', compact('cardapio'));
     }
 
-    /**
-     * Update the specified cardapio in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Cardapio  $cardapio
-     * @return \Illuminate\Routing\Redirector
-     */
-    
-    public function update(Request $request, Cardapio $cardapio)
+
+    public function update(CardapioRequest $request, Cardapio $cardapio)
     {
-        $cardapioData = $request->validate([
-            'nome'         => 'required|max:60',
-            'valor'        => 'required|regex:/\d{1,6}(\,\d{0,2})/',
-            'ingredientes' => 'required|max:500',
-            'status'       => 'required|in:pendente,pronto',
-        ]);
-
+        
+        $cardapioData = $request->validated();
         $cardapioData['valor'] = str_replace(',', '.', $cardapioData['valor']);
-
-      
 
         $cardapio->update($cardapioData);
 
@@ -116,16 +61,8 @@ class CardapioController extends Controller
     }
 
 
-    /**
-     * Remove the specified cardapio from storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Cardapio  $cardapio
-     * @return \Illuminate\Routing\Redirector
-     */
     public function destroy(Request $request, Cardapio $cardapio)
     {
-        //$this->authorize('delete', $cardapio);
 
         $request->validate(['cardapio_id' => 'required']);
 
