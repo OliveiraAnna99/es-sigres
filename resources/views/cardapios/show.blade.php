ee25ad0961303d1e97030424534b7557b916b25a
israelsilva282 <israelcostas@hotmail.com>
2023-07-06 13:29:07 -0300

    refactor: adicionado textarea (#136)

--- resources/views/cardapios/show.blade.php
+++ resources/views/cardapios/show.blade.php
@@ -6,39 +6,39 @@
 
 <div class="cardContainer">
     <div class="cardContainerContent">
-        <div class="cardBody">
-          
-            <div>
-                <p class="funcTitle">{{ __('cardapio.name') }}</p>
-                <p>{{ $cardapio->nome }}</p>
-            </div>
-            <div>
-                <p class="funcTitle">{{ __('cardapio.ingredientes') }}</p>
-                <p>{{ $cardapio->ingredientes }}</p>
-            </div>
-           
-            <div>
-                <p class="funcTitle">{{ __('cardapio.valor') }}</p>
-                <p>{{ $cardapio->valor }}</p>
-            </div>
-           
-        </div>
-        <div class="cardFooter">
-            @can('cardapio.update')
-            <a href="{{ route('cardapios.edit', $cardapio) }}" class="btn btnSecondary">
-                <p>{{__('cardapio.edit')}}</p>
-            </a>
-            @endcan
-            @can('cardapio.delete')
+        <div class="cardShow">
+            <div class="cardBody">
+                <div>
+                    <p class="funcTitle">{{ __('cardapio.name') }}</p>
+                    <p>{{ $cardapio->nome }}</p>
+                </div>
+                <div>
+                    <p class="funcTitle">{{ __('cardapio.ingredientes') }}</p>
+                    <p>{{ $cardapio->ingredientes }}</p>
+                </div>
 
-            <form method="POST" action="{{ route('cardapios.destroy', $cardapio) }}" accept-charset="UTF-8" class="del-form float-right" style="display: inline;">
-                {{ csrf_field() }} {{ method_field('delete') }}
-                <input name="cardapio_id" type="hidden" value="{{ $cardapio->id }}">
-                <button type="submit" class="btn btnPrimary">{{__('cardapio.delete')}}</button>
-            </form>
-            @endcan
+                <div>
+                    <p class="funcTitle">{{ __('cardapio.valor') }}</p>
+                    <p>{{ $cardapio->valor }}</p>
+                </div>
 
+            </div>
+            <div class="cardFooter">
+                @can('cardapio.update')
+                <a href="{{ route('cardapios.edit', $cardapio) }}" class="btn btnSecondary">
+                    <p>{{__('cardapio.edit')}}</p>
+                </a>
+                @endcan
+                @can('cardapio.delete')
+
+                <form method="POST" action="{{ route('cardapios.destroy', $cardapio) }}" accept-charset="UTF-8" class="del-form float-right" style="display: inline;">
+                    {{ csrf_field() }} {{ method_field('delete') }}
+                    <input name="cardapio_id" type="hidden" value="{{ $cardapio->id }}">
+                    <button type="submit" class="btn btnPrimary">{{__('cardapio.delete')}}</button>
+                </form>
+                @endcan
+
+            </div>
         </div>
     </div>
-</div>
-@endsection
+    @endsection
--- resources/views/pedidos/create.blade.php
+++ resources/views/pedidos/create.blade.php
@@ -13,8 +13,7 @@
       <div class="cardBody">
         <div class="formGroup">
           <label for="cardapio_id">{{ __('pedido.cardapio_id') }} <span>*</span></label>
-          <select id="cardapio_id" class="form-control{{ $errors->has('cardapio_id') ? ' is-invalid' : '' }}"
-            name="cardapio_id" required>
+          <select id="cardapio_id" class="form-control{{ $errors->has('cardapio_id') ? ' is-invalid' : '' }}" name="cardapio_id" required>
             <option value="">Selecione um cardápio</option>
             @foreach($cardapios as $cardapio)
             <option value="{{$cardapio->id}}">{{$cardapio->nome}}</option>
@@ -27,10 +26,8 @@
         <div class="formGroup">
           <div>
             <label for="status">{{ __('pedido.status') }} <span>*</span></label>
-            <select id="status" class="form-control{{ $errors->has('status') ? ' is-invalid' : '' }}" name="status"
-              required>
-              <option value="">Selecione o status</option>
-              <option value="1">Em andamento</option>
+            <select id="status" class="form-control{{ $errors->has('status') ? ' is-invalid' : '' }}" name="status" required>
+              <option selected value="1">Em andamento</option>
               <option value="2">Concluído</option>
             </select>
             {!! $errors->first('status', '<span class="invalid-feedback" role="alert">:message</span>') !!}
@@ -41,21 +38,17 @@
         <div class="formGroup ">
           <div>
             <label for="numero_mesa">{{ __('pedido.numero_mesa') }} <span>*</span></label>
-            <input id="numero_mesa" type="number" min="1"
-              class="form-control{{ $errors->has('numero_mesa') ? ' is-invalid' : '' }}" name="numero_mesa"
-              value="{{ old('numero_mesa') }}" required>
+            <input id="numero_mesa" type="number" min="1" class="form-control{{ $errors->has('numero_mesa') ? ' is-invalid' : '' }}" name="numero_mesa" value="{{ old('numero_mesa') }}" required>
             {!! $errors->first('numero_mesa', '<span class="invalid-feedback" role="alert">:message</span>') !!}
           </div>
         </div>
 
-        <div class="formGroup ">
-      <div>
-        <label for="obs">{{ __('pedido.obs') }} <span>*</span></label>
-        <input id="obs" type="text" rows="4" cols="50" class="form-control{{ $errors->has('obs') ? ' is-invalid' : '' }}"
-          name="obs" value="{{ old('obs') }}" required>
-        {!! $errors->first('obs', '<span class="invalid-feedback" role="alert">:message</span>') !!}
-      </div>
-    </div>
+
+        <div class="formGroup">
+          <label for="obs">{{ __('pedido.obs') }} <span>*</span></label>
+          <textarea id="obs" type="text" rows="10" class="form-control{{ $errors->has('obs') ? ' is-invalid' : '' }}" name="obs" value="{{ old('obs') }}" required>
+          {!! $errors->first('obs', '<span class="invalid-feedback" role="alert">:message</span>') !!}</textarea>
+        </div>
 
 
       </div>
