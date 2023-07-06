db99863624afeb3af5d9f49a3aae11e4bad8be05
israelsilva282 <israelcostas@hotmail.com>
2023-07-06 12:05:39 -0300

    fix: mudança na estrutura da página show do estoque (#136)

--- resources/views/estoques/show.blade.php
+++ resources/views/estoques/show.blade.php
@@ -4,45 +4,27 @@
 
 @section('content')
 <div class="cardContainerShow">
-  <div class="cardShowStock"> {{ __('estoque.detail') }}
-    <div class="containerShowTable">
-      <table class="table">
-        <tbody>
-          <tr>
-            <th>{{ __('estoque.item') }}</th>
-            <th>{{ __('estoque.quant') }}</th>
-            <th>{{ __('estoque.date') }}</th>
-          </tr>
-          <tr>
-            <td>{{ $estoque->item }}</td>
-            <td>{{ $estoque->quant }}</td>
-            <td>{{ $estoque->date }}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-    <div class="btnShowStock">
-
-      @can('estoque.read')
-      <a href="{{ route('estoques.index') }}" class="btn btnSecondary">{{ __('estoque.back_to_index') }}</a>
-      @endcan
-
-      @can('estoque.update')
-      <a href="{{ route('estoques.edit', $estoque) }}" id="edit-estoque-{{ $estoque->id }}"
-        class="btn btnSecondary">{{ __('Atualizar item') }}</a>
-      @endcan
-
-      @can('estoque.delete')
-      <form method="POST" action="{{ route('estoques.destroy', $estoque) }}" accept-charset="UTF-8"
-        class="del-form float-right">
-        {{ csrf_field() }} {{ method_field('delete') }}
-        <input name="estoque_id" type="hidden" value="{{ $estoque->id }}">
-        <button type="submit" class="btn btnPrimary">{{__('estoque.delete')}}</button>
-      </form>
-      @endcan
-
-    </div>
+  <div class="containerShowTable">
+    <table class="table">
+      <tbody>
+        <tr>
+          <th>{{ __('estoque.item') }}</th>
+          <th>{{ __('estoque.quant') }}</th>
+          <th>{{ __('estoque.date') }}</th>
+        </tr>
+        <tr>
+          <td>{{ $estoque->item }}</td>
+          <td>{{ $estoque->quant }}</td>
+          <td>{{ $estoque->date }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
+
+  @can('estoque.read')
+  <a href="{{ route('estoques.index') }}" class="btn btnSecondary">{{ __('estoque.back_to_index') }}</a>
+  @endcan
+</div>
 </div>
 
 
