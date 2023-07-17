2719007dd72a02e251deecc0114b86baf00364ab
isazvdd <isadora.azevedo.700@ufrn.edu.br>
2023-07-17 12:29:20 -0300

    update sonar

--- app/Http/Controllers/CardapioController.php
+++ app/Http/Controllers/CardapioController.php
@@ -10,17 +10,17 @@ use Illuminate\Http\Request;
 
 class CardapioController extends Controller
 {
-    
+
     public function index()
     {
         $cardapioQuery = Cardapio::query();
-        $cardapioQuery->where('nome', 'like', '%'.request('q').'%');
+        $cardapioQuery->where('nome', 'like', '%' . request('q') . '%');
         $cardapios = $cardapioQuery->paginate(25);
 
         return view('cardapios.index', compact('cardapios'));
     }
 
- 
+
     public function create()
     {
 
@@ -28,32 +28,32 @@ class CardapioController extends Controller
         return view('cardapios.create', compact('estoques'));
     }
 
-   
-  
+
+
     public function store(CardapioRequest $request)
     {
         $validatedData = $request->validated();
-    
+
         // Substitui ',' por '.' em 'valor'
         $validatedData['valor'] = str_replace(',', '.', $validatedData['valor']);
-    
+
         $cardapio = Cardapio::create($validatedData);
         $estoqueIds = $request->input('estoque_id');
-        
+
         foreach ($estoqueIds as $estoqueId) {
             // Obter o estoque pelo ID
             $estoque = Estoque::find($estoqueId);
-            
+
             if ($estoque) {
                 // Diminuir a quantidade disponível em 1
                 $estoque->quant -= 1;
                 $estoque->save();
             }
         }
-    
+
         return redirect()->route('cardapios.show', $cardapio);
     }
-    
+
 
     public function show(Cardapio $cardapio)
     {
@@ -61,7 +61,7 @@ class CardapioController extends Controller
         return view('cardapios.show', compact('cardapio', 'estoques'));
     }
 
-  
+
     public function edit(Cardapio $cardapio)
     {
         $estoques = Estoque::all();
@@ -71,51 +71,50 @@ class CardapioController extends Controller
 
 
     public function update(CardapioRequest $request, Cardapio $cardapio)
-{
-    $validatedData = $request->validated();
+    {
+        $validatedData = $request->validated();
 
-    // Substitui ',' por '.' em 'valor'
-    $validatedData['valor'] = str_replace(',', '.', $validatedData['valor']);
+        // Substitui ',' por '.' em 'valor'
+        $validatedData['valor'] = str_replace(',', '.', $validatedData['valor']);
 
-    $cardapio->update($validatedData);
-    $estoqueIds = $request->input('estoque_id');
+        $cardapio->update($validatedData);
+        $estoqueIds = $request->input('estoque_id');
 
-    // Restaura as quantidades originais dos estoques associados ao cardápio
-    foreach ($cardapio->estoques as $estoque) {
-        $estoque->quant += 1;
-        $estoque->save();
-    }
-
-    // Atualiza os estoques associados ao cardápio com as novas seleções
-    foreach ($estoqueIds as $estoqueId) {
-        $estoque = Estoque::find($estoqueId);
-
-        if ($estoque) {
-            $estoque->quant -= 1;
+        // Restaura as quantidades originais dos estoques associados ao cardápio
+        foreach ($cardapio->estoques as $estoque) {
+            $estoque->quant += 1;
             $estoque->save();
         }
-    }
 
-    return redirect()->route('cardapios.show', $cardapio);
-}
+        // Atualiza os estoques associados ao cardápio com as novas seleções
+        foreach ($estoqueIds as $estoqueId) {
+            $estoque = Estoque::find($estoqueId);
+
+            if ($estoque) {
+                $estoque->quant -= 1;
+                $estoque->save();
+            }
+        }
+
+        return redirect()->route('cardapios.show', $cardapio);
+    }
 
 
 
     public function destroy(Request $request, Cardapio $cardapio)
     {
         $request->validate(['cardapio_id' => 'required']);
-    
+
         if ($request->get('cardapio_id') == $cardapio->id) {
             // Remover as referências na tabela 'cardapio_estoque'
             $cardapio->estoques()->detach();
-    
+
             // Excluir o cardápio
             $cardapio->delete();
-    
+
             return redirect()->route('cardapios.index');
         }
-    
+
         return back();
     }
-    
 }
